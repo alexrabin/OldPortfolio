a95d079130365884f467a5ee93f05e10fb5d4bad
Alex Rabin <alexrabin@me.com>
2022-04-03 09:36:11 -0700

    Updated work experience and fixed typos

--- src/components/ExperienceSection/WorkSection/index.js
+++ src/components/ExperienceSection/WorkSection/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React from "react";
 
 export default function WorkSection(props) {
-    const {title, work, workLink, dates} = props;
-  return <div>
-        <h4>{title} <a href={workLink} target="_blank" className='ext-link'>@ {work}</a></h4>
-        <p className='mono-font mb-1'>{dates}</p>
-        <div className='mono-font' >{props.children}</div>
-  </div>;
+  const { title, titles, work, workLink, dates } = props;
+  return (
+    <div>
+      {title && (
+        <h4>
+          {title}{" "}
+          <a href={workLink} target="_blank" className="ext-link">
+            @ {work}
+          </a>
+        </h4>
+      )}
+      {titles && (
+        <div>
+          <h4>
+            <a href={workLink} target="_blank" className="ext-link">
+              {work}
+            </a>
+          </h4>
+          {titles.map((data, key) => {
+            return (
+              <div key={key}>
+                <h4>{data.title}</h4>
+                <p className="mono-font mb-1">{data.date}</p>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      {dates && <p className="mono-font mb-1">{dates}</p>}{" "}
+      <div className="mono-font">{props.children}</div>
+    </div>
+  );
 }
--- src/components/ExperienceSection/index.js
+++ src/components/ExperienceSection/index.js
@@ -1,75 +1,149 @@
-import React from 'react';
-import Divider from '../Divider';
-import '../../styles/pills.css';
-import { Tab, Nav, Row, Col } from 'react-bootstrap';
-import WorkSection from './WorkSection';
+import React from "react";
+import Divider from "../Divider";
+import "../../styles/pills.css";
+import { Tab, Nav, Row, Col } from "react-bootstrap";
+import WorkSection from "./WorkSection";
 
-export default function ExperienceSection({theme, styles}) {
-  return <div id='experience' className='mx-auto' style={{width:"95%", marginTop:50}}>
-    <div className="row justify-content-start align-items-center mb-4">
+export default function ExperienceSection({ theme, styles }) {
+  return (
+    <div
+      id="experience"
+      className="mx-auto"
+      style={{ width: "95%", marginTop: 50 }}
+    >
+      <div className="row justify-content-start align-items-center mb-4">
         <div className="col-md-6 col-lg-4">
-            <h1 className={`${styles.textColor}`}>Where I've Worked</h1>
+          <h1 className={`${styles.textColor}`}>Where I've Worked</h1>
         </div>
         <div className="col-md-6 col-lg-8">
-            <Divider styles={styles}/>
+          <Divider styles={styles} />
         </div>
+      </div>
+      <Tab.Container id="work-container" defaultActiveKey="harkins">
+        <Row className="flex-sm-row-reverse">
+          <Col sm={4} className="mb-3">
+            <Nav variant="pills" className="flex-column text-left">
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="harkins"
+                  className={theme === "dark" ? "dark" : "light"}
+                >
+                  Harkins Theatres
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="ameriprise"
+                  className={theme === "dark" ? "dark" : "light"}
+                >
+                  Ameriprise Financial
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="parrotreports"
+                  className={theme === "dark" ? "dark" : "light"}
+                >
+                  Parrot Reports
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="persokey"
+                  className={theme === "dark" ? "dark" : "light"}
+                >
+                  PersoKey
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={8} className="mb-3">
+            <Tab.Content>
+              <Tab.Pane eventKey="harkins" className={`${styles.textColor}`}>
+                <WorkSection
+                  titles={[
+                    {
+                      title: "Systems Developer 1",
+                      date: "March 2022 - Present",
+                    },
+                    {
+                      title: "Contracted React Developer",
+                      date: "December 2021 - March 2022",
+                    },
+                  ]}
+                  work={"Harkins Theatres"}
+                  workLink={"https://www.harkins.com"}
+                >
+                  <li>
+                    Write modern, beautiful, and maintainable code for several
+                    internal projects
+                  </li>
+                  <li>
+                    Work with a variety of different languages, platforms, and
+                    frameworks such as JavaScript, TypeScript, React, React
+                    Native, Bootstrap, Styled Components, and Chakra UI{" "}
+                  </li>
+                  <li>
+                    Communicate with a team of developers, business system
+                    analysts, and project managers on a daily basis
+                  </li>
+                </WorkSection>
+              </Tab.Pane>
+              <Tab.Pane eventKey="ameriprise" className={`${styles.textColor}`}>
+                <WorkSection
+                  title={"Technology Intern"}
+                  work={"Ameriprise Financial"}
+                  workLink={"https://www.ameriprise.com"}
+                  dates={"May 2020 - August 2020"}
+                >
+                  <li>
+                    Worked on a small team to build a dashboard website to show
+                    all customer's financial transactions
+                  </li>
+                  <li>Programmed in JavaScript, React, and Python </li>
+                </WorkSection>
+              </Tab.Pane>
+              <Tab.Pane
+                eventKey="parrotreports"
+                className={`${styles.textColor}`}
+              >
+                <WorkSection
+                  title={"Head Full Stack Developer"}
+                  work={"Parrot Reports"}
+                  workLink={
+                    "https://www.linkedin.com/company/parrot-reports/about/"
+                  }
+                  dates={"July 2019 - January 2020"}
+                >
+                  <li>
+                    Created a service allowing veterinarians to make audiovisual
+                    reports for their clients
+                  </li>
+                  <li>
+                    Designed, implemented and maintained the mobile app,
+                    website, and server
+                  </li>
+                  <li>The Full Stack: Flutter, Angular, and Firebase</li>
+                </WorkSection>
+              </Tab.Pane>
+              <Tab.Pane eventKey="persokey" className={`${styles.textColor}`}>
+                <WorkSection
+                  title={"Junior Mobile Developer"}
+                  work={"Persokey"}
+                  workLink={"https://www.linkedin.com/company/persokey/about/"}
+                  dates={"November 2016 - August 2017"}
+                >
+                  <li>
+                    Worked closely with the CTO to design and implement a
+                    personal identification platform
+                  </li>
+                  <li>Created multiple iOS apps connecting to AWS Lambda</li>
+                </WorkSection>
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
     </div>
-    <Tab.Container id="work-container" defaultActiveKey="harkins">
-  <Row className='flex-sm-row-reverse'>
-  <Col sm={4} className='mb-3'>
-      <Nav variant="pills" className="flex-column text-left">
-        <Nav.Item>
-          <Nav.Link eventKey="harkins" className={theme === 'dark' ? 'dark' : 'light'}>Harkins Theatres</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="amerprise" className={theme === 'dark' ? 'dark' : 'light'}>Amerprise Financial</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="parrotreports" className={theme === 'dark' ? 'dark' : 'light'}>Parrot Reports</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="persokey" className={theme === 'dark' ? 'dark' : 'light'}>PersoKey</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </Col>
-    <Col sm={8} className='mb-3' >
-      <Tab.Content>
-        <Tab.Pane eventKey="harkins" className={`${styles.textColor}`}>
-
-            <WorkSection title={'React Developer'} work={"Harkins Theatres"} workLink={"https://www.harkins.com"} dates={"December 2021 - Present"}>
-              <li>Write modern, beautiful, and maintainable code for several internal projects</li>
-              <li>Work with a variety of different languages, platforms, and frameworks such as JavaScript, TypeScript, React, React Native, Bootstrap, and Chakra UI </li>
-              <li>Communicate with a team of developers, business system analysts, and project managers on a daily basis</li>
-            </WorkSection>
-            
-            
-        </Tab.Pane>
-        <Tab.Pane eventKey="amerprise" className={`${styles.textColor}`}>
-        <WorkSection title={'Technology Intern'} work={"Amerprise Financial"} workLink={"https://www.ameriprise.com"} dates={"May 2020 - August 2020"}>
-              <li>Worked on a small team to build a dashboard website to show all customer's financial transactions</li>
-              <li>Programmed in JavaScript, React, and Python </li>
-            </WorkSection>
-        </Tab.Pane>
-        <Tab.Pane eventKey="parrotreports" className={`${styles.textColor}`}>
-           
-            
-        <WorkSection title={'Head Full Stack Developer'} work={"Parrot Reports"} workLink={"https://www.linkedin.com/company/parrot-reports/about/"} dates={"July 2019 - January 2020"}>
-              <li>Created a service allowing veterinarians to make audiovisual reports for their clients</li>
-              <li>Designed, implemented and  maintained the mobile app, website, and server</li>
-              <li>The Full Stack: Flutter, Angular, and Firebase</li>
-            </WorkSection>
-        </Tab.Pane>
-        <Tab.Pane eventKey="persokey" className={`${styles.textColor}`}>
-           
-            
-           <WorkSection title={'Junior Mobile Developer'} work={"Persokey"} workLink={"https://www.linkedin.com/company/persokey/about/"} dates={"Novemeber 2016 - August 2017"}>
-                 <li>Worked closely with the CTO to design and implement a personal identification platform</li>
-                 <li>Created multiple iOS apps connecting to AWS Lambda</li>
-               </WorkSection>
-           </Tab.Pane>
-      </Tab.Content>
-    </Col>
-  </Row>
-</Tab.Container>
-  </div>;
+  );
 }
