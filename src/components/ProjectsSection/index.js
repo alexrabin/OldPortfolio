1cde15e568d18542e4abfec19bc224d3403a1853
Alex Rabin <alexrabin@me.com>
2022-11-24 15:43:20 -0700

    Updated projects

--- src/components/ProjectsSection/index.js
+++ src/components/ProjectsSection/index.js
@@ -1,10 +1,47 @@
 import React from "react";
 import Divider from "../Divider";
 import Project from "./Project";
-import letsdothisicon from "../../assets/letsdothisicon.png";
 import dragazon from "../../assets/dragazon.png";
 import { FaLock } from "react-icons/fa";
-export default function ProjectsSection({ theme, styles }) {
+
+const projects = [
+  {
+    title: 'Rickest Rick',
+    codeLink: 'https://github.com/alexrabin/RickestRick',
+    projectLink: 'https://rickest-rick.vercel.app',
+    imageLink: 'https://rickest-rick.vercel.app/apple-touch-icon.png',
+    description: "View Characters, Episodes, and Locations from the TV show, Rick & Morty. Uses Next.js, MUI, & the Rick & Morty API."
+  },
+  {
+    title: 'Happy Paws',
+    codeLink: 'https://github.com/alexrabin/HappyPaws',
+    projectLink: 'https://happypaws.vercel.app',
+    imageLink: 'https://happypaws.vercel.app/apple-touch-icon.png',
+    description: 'Your home for all your happy paws :) Uses Next.js and The Dog API.'
+  },
+  {
+    title: 'native_drag_n_drop',
+    codeLink: 'https://github.com/alexrabin/FlutterNativeDragAndDrop',
+    projectLink: 'https://pub.dev/packages/native_drag_n_drop',
+    imageLink: 'https://user-images.githubusercontent.com/15949910/150895221-6a4e58f8-4238-43e6-8549-4e626389985b.png',
+    description: 'A package that allows developers to add native drag and drop support into their Flutter app.'
+  },
+  {
+    title: 'Dragazon',
+    codeLink: 'https://github.com/alexrabin/dragazon/tree/testing',
+    projectLink: 'https://dragazon.herokuapp.com',
+    imageLink: dragazon,
+    description:' A store for all your dragon related needs! Dragazon is a MERN  e-commerce application created by Lonnie Mitchell, Chris Garrison, and I.'
+  },
+  {
+    title: 'Password Generator',
+    codeLink: 'https://github.com/alexrabin/password-generator',
+    projectLink: 'https://alexrabin.github.io/password-generator/',
+    imageIcon:<FaLock size={80} />,
+    description:'A password generator built with React and Bootstrap.'
+  },
+]
+export default function ProjectsSection({ styles }) {
   return (
     <div
       id="projects"
@@ -20,77 +57,20 @@ export default function ProjectsSection({ theme, styles }) {
         </div>
       </div>
       <div id="the-projects">
-      <Project
-          left
+        {projects.map((project, i) => {
+          return <Project
+          left={i % 2 === 0}
           styles={styles}
-          title={"Happy Paws"}
-          codeLink={"https://github.com/alexrabin/HappyPaws"}
-          projectLink="https://happypaws.vercel.app"
-          imageLink="https://happypaws.vercel.app/apple-touch-icon.png"
+          title={project.title}
+          codeLink={project.codeLink}
+          projectLink={project.projectLink}
+          imageLink={project.imageLink}
+          imageIcon={project.imageIcon}
         >
-          <p style={{ color: styles.accentTextColor }} className="mono-font">
-          Your home for all your happy paws :) Uses Next.js and The Dog API.
-          </p>
+          <p style={{ color: styles.accentTextColor }} className={`mono-font ${i % 2 === 0 ? 'text-start' : 'text-end'} w-100`}>
+         {project.description}</p>
         </Project>
-        <Project
-          left={false}
-          styles={styles}
-          title={"native_drag_n_drop"}
-          codeLink={"https://github.com/alexrabin/FlutterNativeDragAndDrop"}
-          projectLink="https://pub.dev/packages/native_drag_n_drop"
-          imageLink="https://user-images.githubusercontent.com/15949910/150895221-6a4e58f8-4238-43e6-8549-4e626389985b.png"
-        >
-          <p style={{ color: styles.accentTextColor }} className="mono-font">
-            A package that allows developers to add native drag and drop support
-            into their Flutter app.
-          </p>
-        </Project>
-        <Project
-          left
-          styles={styles}
-          title={"Dragazon"}
-          codeLink={"https://github.com/alexrabin/dragazon/tree/testing"}
-          projectLink="https://dragazon.herokuapp.com"
-          imageLink={dragazon}
-        >
-          <p
-            className="text-end mono-font"
-            style={{ color: styles.accentTextColor }}
-          >
-            A store for all your dragon related needs! Dragazon is a MERN
-            e-commerce application created by Lonnie Mitchell, Chris Garrison,
-            and I.
-          </p>
-        </Project>
-        <Project
-          left={false}
-          styles={styles}
-          title={"Password Generator"}
-          codeLink={"https://github.com/alexrabin/password-generator"}
-          projectLink="https://alexrabin.github.io/password-generator/"
-          imageIcon={<FaLock size={80} />}
-        >
-          <p style={{ color: styles.accentTextColor }} className="mono-font">
-            A password generator built with React and Bootstrap.{" "}
-          </p>
-        </Project>
-        <Project
-          left
-          styles={styles}
-          title={"Let's Do This"}
-          imageLink={letsdothisicon}
-        >
-          <p
-            className="text-end mono-font"
-            style={{ color: styles.accentTextColor }}
-          >
-            Back in 2015, my friend, Jacob and I created our first iOS app
-            called Let's Do This. The app was a car rampage game, where the user
-            would have to dodge incoming cars and shoot those cars to stay
-            alive. The user could collect coins and then save up those coins to
-            buy different types of cars.
-          </p>
-        </Project>
+        })}
       </div>
     </div>
   );
